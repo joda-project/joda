e462d4513cb1e0add433fcc8ab2840d7ab78e9b8
Tadej Novak <tadej@tano.si>
2017-02-08 11:03:01 +0100

    Load bulma from main node_modules when in production

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,11 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
+  var sassInclude = 'node_modules';
+  if (EmberApp.env() === 'development') {
+    sassInclude = 'node_modules/joda-core/node_modules';
+  }
+
   var app = new EmberApp(defaults, {
     fingerprint: {
       extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map', 'svg'],
@@ -11,8 +16,9 @@ module.exports = function(defaults) {
     storeConfigInMeta: false,
     sassOptions: {
       extension: 'sass',
+      onlyIncluded: true,
       includePaths: [
-        'node_modules/joda-core/node_modules'
+        sassInclude
       ]
     }
   });
