19cc5b7e5a4f28de8ec30e1c5ad9d62e154276ef
Tadej Novak <tadej@tano.si>
2017-02-26 13:10:15 +0100

    Add webpage assets

---
+++ assets/gen-webpage-assets.sh
@@ -0,0 +1,6 @@
+# Logo
+inkscape --export-png huge.png -w 300 logo-white.svg
+inkscape --export-png huge@2x.png -w 600 logo-white.svg
+
+inkscape --export-png huge-mobile.png -w 200 logo-white.svg
+inkscape --export-png huge-mobile@2x.png -w 400 logo-white.svg
