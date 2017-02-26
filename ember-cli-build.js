7deddfe26e1724f015577d9b6d5bf64e0bd4adeb
Tadej Novak <tadej@tano.si>
2017-02-26 12:36:04 +0100

    Move styles to main Joda repository

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,11 +3,6 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var sassInclude = 'node_modules';
-  if (EmberApp.env() === 'development') {
-    sassInclude = 'node_modules/joda-core/node_modules';
-  }
-
   var app = new EmberApp(defaults, {
     fingerprint: {
       extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map', 'svg'],
@@ -21,7 +16,7 @@ module.exports = function(defaults) {
       extension: 'sass',
       onlyIncluded: true,
       includePaths: [
-        sassInclude
+        'node_modules'
       ]
     }
   });
