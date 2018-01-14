241f9b35266d1d7bccf68fed6996e5a12a875161
Tadej Novak <tadej@tano.si>
2018-01-14 11:23:50 +0100

    Add fingerprinting to fonts

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
-      extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map', 'svg'],
+      extensions: [
+        'js', 'css', 'png', 'jpg', 'gif', 'map', 'svg',
+        'eot', 'otf', 'ttf', 'woff', 'woff2'
+      ],
       replaceExtensions: ['html', 'css', 'js', 'xml', 'json']
     },
     storeConfigInMeta: false,
